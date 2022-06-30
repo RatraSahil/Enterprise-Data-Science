8047ce16b00766ff50dc4378beb8b767cf857352
ratra <ratra@rhrk.uni-kl.de>
2022-06-30 14:35:12 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Key idea of the project is to analyse and visualize Covid-19 data, and make sense of the vast amount of numbers we have at our disposal from various countries and regions.',
+    author='Sahil Ratra',
+    license='',
+)
